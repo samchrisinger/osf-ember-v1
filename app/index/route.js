dedd16c080785dc15937cb6d160956de6fd9a6b0
Chris Seto <chriskseto@gmail.com>
2015-08-11 16:15:03 -0700

    Update index page

--- app/index/route.js
+++ app/index/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+ actions: {
+    willTransition: function(transition) {
+        if (session.isAuthenticated) {
+            transition.redirectTo('dashboard');
+        }
+    }
+  }
 });
