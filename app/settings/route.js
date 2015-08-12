1c35d01ad4b61dd8a9f4dfe4272b3bab5628ca7d
Chris Seto <chriskseto@gmail.com>
2015-08-12 11:48:21 -0700

    Update settings routes

--- app/settings/route.js
+++ app/settings/route.js
@@ -1,7 +1,26 @@
 import Ember from 'ember';
 
+var PAGE_LINKS = [
+{
+    route: 'settings.account',
+    title: 'Profile Information'
+},
+{
+    route: 'settings.addons',
+    title: 'Configure Add-on Accounts'
+},
+{
+    route: 'settings.notifications',
+    title: 'Notifications'
+}
+];
+
+
 export default Ember.Route.extend({
   model() {
-    console.log('settings');
+        console.log('settings');
+      return {
+          links: PAGE_LINKS
+      };
   }
 });
