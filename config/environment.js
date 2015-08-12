0c8a4bcf1303691dc344fdb2c5f63b41c15876d7
Chris Seto <chriskseto@gmail.com>
2015-08-12 15:15:52 -0700

    Add CAS_HOST

--- config/environment.js
+++ config/environment.js
@@ -35,6 +35,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.CAS_HOST = 'http://localhost:8080';
     ENV.APP.API_HOST = 'http://localhost:8000';
     ENV.APP.API_NAMESPACE = 'v2';
   }
